a1d6c7dd490c75e3b503f146361596c8a17b7fd4
jaejunglee <leejj4487@gmail.com>
2020-04-27 17:32:31 +0900

    Updates

---
+++ precache-manifest.29dd5572f9947770f1fb565514f1cfb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a473970e31c2ea26ddfd0f6a84dfc6a",
+    "url": "/flutter_crm/index.html"
+  },
+  {
+    "revision": "69a7b022dcd12089dbe6",
+    "url": "/flutter_crm/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d898cc4962b32071cd1e",
+    "url": "/flutter_crm/static/css/main.ecf0da8d.chunk.css"
+  },
+  {
+    "revision": "69a7b022dcd12089dbe6",
+    "url": "/flutter_crm/static/js/2.01b97a37.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/flutter_crm/static/js/2.01b97a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d898cc4962b32071cd1e",
+    "url": "/flutter_crm/static/js/main.6749fcb5.chunk.js"
+  },
+  {
+    "revision": "fafb16812aa6c99a7607",
+    "url": "/flutter_crm/static/js/runtime-main.70a9e126.js"
+  }
+]);
